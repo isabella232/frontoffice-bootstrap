7f176eb8e66bb4c9b1910d561ab1b4e87599ab55
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-08-30 18:46:29 -0300

    Added generic router

--- src/app/screens/Dashboard/screens/Home/index.js
+++ src/app/screens/Dashboard/screens/Home/index.js
@@ -3,14 +3,15 @@ import React from 'react';
 import logo from './assets/logo.svg';
 import styles from './styles.module.scss';
 
-function Home() {
+import structure from '~constants/structure';
+
+function List(props) {
+  const data = structure.find(model => props.match.path.slice(1) === model.endpoint);
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt="logo" />
-        <p className={styles.text}>
-          Edit <code>src/app/index.js</code> and save to reload.
-        </p>
+        <p className={styles.text}>{data.name}</p>
         <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
@@ -19,4 +20,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default List;
