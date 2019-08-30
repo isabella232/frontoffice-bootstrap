b62f441e6ebc34aad5240caa2eb96388974277ae
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-08-30 19:36:50 -0300

    Moved to react component

--- src/app/screens/Dashboard/screens/List/index.js
+++ src/app/screens/Dashboard/screens/List/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import logo from './assets/logo.svg';
 import styles from './styles.module.scss';
 
 import structure from '~constants/structure';
 
-function List(props) {
-  const data = structure.find(model => props.match.path.slice(1) === model.endpoint);
-  return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo" />
-        <p className={styles.text}>{data.name} list</p>
-        <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class List extends Component {
+  state = {
+    data: {}
+  };
+
+  componentDidMount() {
+    this.setState({
+      data: structure.find(model => this.props.match.path.slice(1) === model.endpoint)
+    });
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <p className={styles.text}>{this.state.data.name} list</p>
+          <a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default List;
