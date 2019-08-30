a9b83719de86c4e2219f0719c7794b24009b454a
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-08-30 18:10:06 -0300

    Added generic router

--- src/app/screens/Dashboard/index.js
+++ src/app/screens/Dashboard/index.js
@@ -5,9 +5,15 @@ import Routes from '../../../constants/routes';
 
 import Home from './screens/Home';
 
+import structure from '~constants/structure';
+
 function Dashboard() {
+  const routes = structure.map(model => model.endpoint);
   return (
     <Switch>
+      {routes.map(route => (
+        <Route key={route} path={`/${route}`} component={Home} />
+      ))}
       <Route exact path={Routes.HOME} component={Home} />
       <Route component={<Redirect to={Routes.HOME} />} />
     </Switch>
---
+++ src/constants/structure.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: 'sports',
+    endpoint: 'sports',
+    attributes: [
+      {
+        name: 'name',
+        type: 'text'
+      }
+    ]
+  },
+  {
+    name: 'matches',
+    endpoint: 'matches',
+    attributes: [
+      {
+        name: 'name',
+        type: 'text'
+      }
+    ]
+  }
+];
