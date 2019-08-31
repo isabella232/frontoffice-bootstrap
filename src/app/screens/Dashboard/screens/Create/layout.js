751f7c9656fb97f8811d364cb35e6c96ad7b3541
Francisco Iglesias <franciscoj.iglesias@hotmail.com>
2019-08-30 21:10:27 -0300

    Added model creation layout

---
+++ src/app/screens/Dashboard/screens/Create/layout.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+import styles from './styles.module.scss';
+
+function CreationLayout({ modelData, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <h1 className="title m-bottom-1">{modelData.name} Creation</h1>
+      {modelData.attributes &&
+        modelData.attributes.map(attribute => (
+          <Field
+            key={attribute.name}
+            className="input"
+            component="input"
+            placeholder={attribute.name}
+            {...attribute}
+          />
+        ))}
+      <div className="full-width row end">
+        <button type="button" className="m-right-1 m-top-1">
+          Cancelar
+        </button>
+        <button type="submit" className="m-right-1 m-top-1">
+          Guardar
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default reduxForm({
+  form: 'creation'
+})(CreationLayout);
